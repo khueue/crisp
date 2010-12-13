deb7f04e9755328f8abc095cbfb440545cf519b7
khueue <khueue@gmail.com>
2010-12-13 21:49:30 +0100

    Cleanup and rearrange.

--- crisp.pl
+++ crisp.pl
@@ -24,35 +24,25 @@ crisp :-
 
 collect_and_run :-
     setof(Module, current_module(Module), Modules),
-    run_all_modules(Modules, stats(0,0), GlobalStats),
+    run_all_modules(Modules, GlobalStats),
     write_summary(GlobalStats).
 
-write_summary(stats(Pass,Fail)) :-
-    nl,
-    write('Summary: '),
-    Total is Pass + Fail,
-    write_ratio(Fail/Total), write(' fail, '),
-    write_ratio(Pass/Total), write(' pass'),
-    nl.
+run_all_modules(Modules, GlobalStats) :-
+    run_all_modules(Modules, stats(0,0), GlobalStats).
 
-run_all_modules([], GlobalStats, GlobalStats).
-run_all_modules([Module|Modules], GlobalStats0, GlobalStats) :-
+run_all_modules([], Stats, Stats).
+run_all_modules([Module|Modules], Stats0, Stats) :-
     suitable_module(Module),
     !,
-    run_module(Module, GlobalStats0, GlobalStats1),
-    run_all_modules(Modules, GlobalStats1, GlobalStats).
-run_all_modules([_Module|Modules], GlobalStats0, GlobalStats) :-
+    run_module(Module, Stats0, Stats1),
+    run_all_modules(Modules, Stats1, Stats).
+run_all_modules([_Module|Modules], Stats0, Stats) :-
     % \+ suitable_module(Module),
-    run_all_modules(Modules, GlobalStats0, GlobalStats).
+    run_all_modules(Modules, Stats0, Stats).
 
 suitable_module(Module) :-
     \+ ignored_module(Module),
     current_predicate(Module:test/2).
-
-run_module(Module, Stats0, Stats) :-
-    write('### Module: '), write(Module), nl,
-    findall(test(Name,Goals), Module:test(Name,Goals), Tests),
-    run_all_tests(Tests, Module, Stats0, Stats).
 
 % This module.
 ignored_module(crisp).
@@ -73,10 +63,15 @@ ignored_module(clpfd).
 ignored_module(prolog).
 ignored_module('SU_messages').
 
+run_module(Module, Stats0, Stats) :-
+    write('### Module: '), write(Module), nl,
+    findall(test(Name,Goals), Module:test(Name,Goals), Tests),
+    run_all_tests(Tests, Module, Stats0, Stats).
+
 run_all_tests([], _Module, GlobalStats, GlobalStats).
 run_all_tests([test(Name,Goals)|Tests], Module, GlobalStats0, GlobalStats) :-
     run_test(test(Name,Goals), Module, TestStats),
-    update_global_stats(GlobalStats0, TestStats, GlobalStats1),
+    update_stats(GlobalStats0, TestStats, GlobalStats1),
     run_all_tests(Tests, Module, GlobalStats1, GlobalStats).
 
 run_test(test(Name,Goals), Module, TestStats) :-
@@ -84,18 +79,58 @@ run_test(test(Name,Goals), Module, TestStats) :-
     run_goals(Goals, Module, TestStats),
     write_stats(TestStats).
 
+update_stats(stats(P0,F0), stats(P1,F1), stats(Pass,Fail)) :-
+    Pass is P0 + P1,
+    Fail is F0 + F1.
+
+run_goals(Goals, Module, TestStats) :-
+    run_goals(Goals, Module, stats(0,0), TestStats).
+
+run_goals([], _Module, Stats, Stats).
+run_goals([true|Goals], Module, Stats0, Stats) :-
+    !,
+    run_goals(Goals, Module, Stats0, Stats).
+run_goals([one:Goal|Goals], Module, Stats0, Stats) :-
+    !,
+    execute_deterministic_test(Goal, Module, Result),
+    write_result(Result, one:Goal),
+    update_stats(Result, Stats0, Stats1),
+    run_goals(Goals, Module, Stats1, Stats).
+run_goals([fail:Goal|Goals], Module, Stats0, Stats) :-
+    !,
+    execute_test(\+(Goal), Module, Result),
+    write_result(Result, fail:Goal),
+    update_stats(Result, Stats0, Stats1),
+    run_goals(Goals, Module, Stats1, Stats).
+run_goals([Goal|Goals], Module, Stats0, Stats) :-
+    % Goal has no special form.
+    execute_test(Goal, Module, Result),
+    write_result(Result, Goal),
+    update_stats(Result, Stats0, Stats1),
+    run_goals(Goals, Module, Stats1, Stats).
+
+execute_deterministic_test(Goal, Module, pass) :-
+    findall(_, Module:Goal, [_ExactlyOneSolution]),
+    !.
+execute_deterministic_test(_Goal, _Module, fail).
+    % Not exactly one solution.
+
+execute_test(Goal, Module, pass) :-
+    Module:call(Goal),
+    !.
+execute_test(_Goal, _Module, fail).
+    % \+ Module:call(Goal).
+
+update_stats(pass, stats(Pass0,Fail), stats(Pass,Fail)) :-
+    Pass is Pass0 + 1.
+update_stats(fail, stats(Pass,Fail0), stats(Pass,Fail)) :-
+    Fail is Fail0 + 1.
+
 write_test_name(Pred/Arity) :-
     !,
     write('- '), write(Pred/Arity).
 write_test_name(Name) :-
     write('- \''), write(Name), write('\'').
-
-run_goals(Goals, Module, TestStats) :-
-    run_goals(Goals, Module, stats(0,0), TestStats).
-
-update_global_stats(stats(P0,F0), stats(P1,F1), stats(Pass,Fail)) :-
-    Pass is P0 + P1,
-    Fail is F0 + F1.
 
 write_stats(stats(Pass,0)) :-
     !,
@@ -123,50 +158,18 @@ write_ratio(Some/All) :-
     !,
     write(Some/All).
 
-run_goals([], _Module, Stats, Stats).
-run_goals([true|Goals], Module, Stats0, Stats) :-
-    !,
-    run_goals(Goals, Module, Stats0, Stats).
-run_goals([one:Goal|Goals], Module, Stats0, Stats) :-
-    !,
-    execute_deterministic_test(Goal, Module, Result),
-    write_result(one:Goal, Result),
-    update_stats(Result, Stats0, Stats1),
-    run_goals(Goals, Module, Stats1, Stats).
-run_goals([fail:Goal|Goals], Module, Stats0, Stats) :-
-    !,
-    execute_test(\+(Goal), Module, Result),
-    write_result(fail:Goal, Result),
-    update_stats(Result, Stats0, Stats1),
-    run_goals(Goals, Module, Stats1, Stats).
-run_goals([Goal|Goals], Module, Stats0, Stats) :-
-    % Goal has no special form.
-    execute_test(Goal, Module, Result),
-    write_result(Goal, Result),
-    update_stats(Result, Stats0, Stats1),
-    run_goals(Goals, Module, Stats1, Stats).
-
-execute_deterministic_test(Goal, Module, pass) :-
-    findall(_, Module:Goal, [_ExactlyOneSolution]),
-    !.
-execute_deterministic_test(_Goal, _Module, fail).
-    % Not exactly one solution.
-
-execute_test(Goal, Module, pass) :-
-    Module:call(Goal),
-    !.
-execute_test(_Goal, _Module, fail).
-    % \+ Module:call(Goal).
-
-write_result(_Goal, pass) :- !.
-write_result(Goal, fail) :-
+write_result(pass, _Goal).
+write_result(fail, Goal) :-
     nl,
     write('    >>> FAIL: '), write(Goal).
 
-update_stats(pass, stats(Pass0,Fail), stats(Pass,Fail)) :-
-    Pass is Pass0 + 1.
-update_stats(fail, stats(Pass,Fail0), stats(Pass,Fail)) :-
-    Fail is Fail0 + 1.
+write_summary(stats(Pass,Fail)) :-
+    nl,
+    write('Summary: '),
+    Total is Pass + Fail,
+    write_ratio(Fail/Total), write(' fail, '),
+    write_ratio(Pass/Total), write(' pass'),
+    nl.
 
 write_prologue :-
     version(Version),
